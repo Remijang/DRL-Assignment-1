13175f37b31951731229c3a3522f4857a3547e0a
Remijang <wuet0829@gmail.com>
2025-03-21 00:45:09 +0800

    test

--- lib.py
+++ lib.py
@@ -11,8 +11,8 @@ import torch.optim as optim
 
 class PyTorchPolicy:
     def __init__(self):
-        self.state_size = (2,  2, 2, 2, 2,  3, 3)
-        self.action_size = 4
+        self.state_size = (2, 2, 2,  2, 2, 2, 2,  3, 3)
+        self.action_size = 6
         self.get_passenger = 0
         self.target = 0
         self.policy = torch.zeros(*(self.state_size), self.action_size)
@@ -23,13 +23,6 @@ class PyTorchPolicy:
         state = self.get_agent_state(obs)
         probs = torch.softmax(self.policy[state], dim=0)
         action = torch.multinomial(probs, 1).item()
-
-        if (obs[0], obs[1]) in self.station:
-            if self.get_passenger == 0 and obs[-2] == 1:
-                action = 4
-            elif self.get_passenger == 1 and obs[-1] == 1:
-                action = 5
-        
         self.update_state(obs, action)
         return action
 
@@ -48,7 +41,7 @@ class PyTorchPolicy:
             coll = 1
         else:
             coll = 2
-        return (self.get_passenger, 
+        return (obs[-2], obs[-1], self.get_passenger, 
                 obs[-5], obs[-6], obs[-4], obs[-3],
                 roww, coll)
 
